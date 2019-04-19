367b4948a0c3501491ee289d91db2b820c8c77ae
Martin Gyurkó <nospam@gyurma.de>
2019-04-19 16:39:18 +0200

    trying out queues

---
+++ tesztqueue.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+
+
+import queue
+
+q = queue.Queue()
+
+for i in range(5):
+    q.put(i)
+
+while not q.empty():
+    print(q.get(), end=' ')
+print()
+for i in range(5):
+    q.put(i)
+q.put('valami')
+q.put(b'valami')
+
+while not q.empty():
+    print(type(q.get()))
+print()
