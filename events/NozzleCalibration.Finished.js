1b903f2029e990e0ce4d7a97917d9c20f10425a0
Martin Gyurkó <info@engyneer.at>
2024-07-25 10:28:28 +0200

    first full jet, pick, alignment and place with table movement using event scripts

---
+++ events/NozzleCalibration.Finished.js
@@ -0,0 +1,8 @@
+print('Move Table to 100');
+
+
+/**
+ * Turn on Down Camera Lights
+ */
+ var Table = machine.getActuatorByName("Table");
+Table.actuate(false);
---
+++ events/NozzleCalibration.Starting.js
@@ -0,0 +1,8 @@
+print('Move Table to 300');
+
+
+/**
+ * Turn on Down Camera Lights
+ */
+ var Table = machine.getActuatorByName("Table");
+Table.actuate(true);
